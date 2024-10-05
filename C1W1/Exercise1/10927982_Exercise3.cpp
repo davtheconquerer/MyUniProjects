3e15cba6baae06a425a4c4071a4d483a57885fb8
davtheconquerer <sion.williams54@gmail.com>
2024-10-05 14:03:31 +0100

    exercise 3 completed

---
+++ C1W1/Exercise1/10927982_Exercise3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <=10; i++) { // iterates through numbers 1 to 10
+        cout << i << endl; // outputs the current iteration
+    }
+}
