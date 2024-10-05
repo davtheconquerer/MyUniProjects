3b6ef82f046821225d543538c41bfc8d3c2c0672
davtheconquerer <sion.williams54@gmail.com>
2024-10-05 13:54:11 +0100

    first exercise completed

---
+++ C1W1/Exercise1/10927982_Exercise1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 7;
+    cout << "sum of " << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    return 0;
+}
