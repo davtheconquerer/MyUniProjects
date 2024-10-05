fab59ed8994bc9a2dfa1cd9df34e8ee1ff91bfa4
davtheconquerer <sion.williams54@gmail.com>
2024-10-05 13:59:09 +0100

    added comments to exercise 2

--- C1W1/Exercise1/10927982_Exercise2.cpp
+++ C1W1/Exercise1/10927982_Exercise2.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "Product of 5 and 3: " << 5 * 3 << endl;
+    cout << "Product of 5 and 3: " << 5 * 3 << endl; // output product 5 x 3
     return 0;
 }
